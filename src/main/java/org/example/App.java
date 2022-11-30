de4fd00ed9a1e6f1c44dcaea8c16a0acfaa23ba9
Kléber <kleber0a0m0@gmail.com>
2022-11-30 20:23:09 -0300

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        LocalDate dataDeNascimento;
+
+        Scanner scanner = new Scanner(System.in);
+
+        String nome,sobrenome;
+
+        System.out.println("Digite o seu nome:");
+        nome = scanner.nextLine();
+
+        System.out.println("Digite o seu sobrenome:");
+        sobrenome = scanner.nextLine();
+
+        System.out.println("Digite a sua data de nascimento:");
+        dataDeNascimento = LocalDate.parse(scanner.nextLine());
+
+        int idade = LocalDate.now().getYear() - dataDeNascimento.getYear();
+
+        System.out.println("Olá "+nome+" "+sobrenome+", sua idade é "+idade);
+
+
+    }
+}
